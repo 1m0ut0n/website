255c1bc899dbebeaf12ca6a2d0c0a655a87f61a2
Gaspard <77894810+1m0ut0n@users.noreply.github.com>
2026-02-02 21:34:52 +0100

    protect email

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "gasprd.net",
-  EMAIL: "reachout@gasprd.net",
+  EMAIL: "reachout[at]gasprd.net",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -25,16 +25,17 @@ export const WORK: Metadata = {
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "github",
-    HREF: "https://github.com/1m0ut0n"
+    HREF: "https://github.com/1m0ut0n",
   },
-  { 
+  {
     NAME: "linkedin",
     HREF: "https://www.linkedin.com/in/gaspard-beunier/",
-  }
+  },
 ];
