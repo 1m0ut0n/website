d36a60926a279d2c6c54054e7a6196a6936f8672
Gaspard <77894810+1m0ut0n@users.noreply.github.com>
2026-02-03 23:19:01 +0100

    update header

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "gasprd.net",
+  NAME: "GASPRD",
   EMAIL: "reachout@gasprd.net",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
