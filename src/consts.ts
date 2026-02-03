99773dc52672160a43f9438b4f9bcc694b486bd1
Gaspard <77894810+1m0ut0n@users.noreply.github.com>
2026-02-03 23:20:00 +0100

    fix website name

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "GASPRD",
+  NAME: "GASPRD.net",
   EMAIL: "reachout@gasprd.net",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
